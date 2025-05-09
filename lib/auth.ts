a8829382e04be917f453b646129f1dd40f9c4bba
heiwa4126 <heiwa4126@gmail.com>
2025-05-09 14:05:43 +0900

    [WIP] fix "should be awaited before using its value" warning

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,8 @@
-import { Lucia } from "lucia";
 import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite";
-import { db } from "./db";
+import { Lucia } from "lucia";
 import { cookies } from "next/headers";
 import { cache } from "react";
+import { db } from "./db";
 
 import type { Session, User } from "lucia";
 import type { DatabaseUser } from "./db";
@@ -30,27 +30,28 @@ export const lucia = new Lucia(adapter, {
 
 export const validateRequest = cache(
 	async (): Promise<{ user: User; session: Session } | { user: null; session: null }> => {
-		const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
-		if (!sessionId) {
-			return {
-				user: null,
-				session: null
-			};
-		}
+			const cookiesInstance = await cookies(); // cookies() を await で取得
+			const sessionId = cookiesInstance.get(lucia.sessionCookieName)?.value ?? null;
+			if (!sessionId) {
+					return {
+							user: null,
+							session: null
+					};
+			}
 
-		const result = await lucia.validateSession(sessionId);
-		// next.js throws when you attempt to set cookie when rendering page
-		try {
-			if (result.session && result.session.fresh) {
-				const sessionCookie = lucia.createSessionCookie(result.session.id);
-				cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-			}
-			if (!result.session) {
-				const sessionCookie = lucia.createBlankSessionCookie();
-				cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-			}
-		} catch {}
-		return result;
+			const result = await lucia.validateSession(sessionId);
+			// next.js throws when you attempt to set cookie when rendering page
+			try {
+					if (result.session && result.session.fresh) {
+							const sessionCookie = lucia.createSessionCookie(result.session.id);
+							cookiesInstance.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+					}
+					if (!result.session) {
+							const sessionCookie = lucia.createBlankSessionCookie();
+							cookiesInstance.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+					}
+			} catch {}
+			return result;
 	}
 );
 
